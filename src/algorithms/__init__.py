5674194d8c3d2b65cf610319d5ac68cd4375200b
Пользователь <levdubogryzov1@gmail.com>
2026-04-26 21:39:41 +0300

    feat: add algorithm validator

--- src/algorithms/__init__.py
+++
@@ -1,3 +0,0 @@
-from .base import BaseAlgorithm
-
-__all__ = ["BaseAlgorithm"]
--- src/algorithms/graph/__init__.py
+++
@@ -1,5 +0,0 @@
-from .dijkstra import Dijkstra
-from .a_star import AStar
-from .bellman_ford import BellmanFord
-
-__all__ = ["Dijkstra", "AStar", "BellmanFord"]
